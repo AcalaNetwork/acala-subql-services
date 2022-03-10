e5ab52f06f638ce316a10966b1cc98b35112acf5
qwer951123 <qiuhao951@gmail.com>
2022-03-10 23:43:10 +0800

    support query stablecoin decimals

--- packages/utils/src/tokens.ts
+++ packages/utils/src/tokens.ts
@@ -1,4 +1,4 @@
-import { AnyApi, forceToCurrencyName, getForeignAssetIdFromName, isDexShareName, isForeignAssetName, isLiquidCrowdloanName, unzipDexShareName } from '@acala-network/sdk-core'
+import { AnyApi, forceToCurrencyName, getForeignAssetIdFromName, getStableAssetPoolIdFromName, isDexShareName, isForeignAssetName, isLiquidCrowdloanName, isStableAssetName, unzipDexShareName } from '@acala-network/sdk-core'
 import { ApiPromise, ApiRx } from '@polkadot/api'
 import { zip, isEmpty } from 'lodash'
 
@@ -36,6 +36,12 @@ export async function getTokenDecimals(api: ApiPromise | ApiRx, token: any) {
     if (isForeignAssetName(name) && api.query.assetRegistry && !tokensDecimals[name]) {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const metadata = (await api.query.assetRegistry.assetMetadatas({ ForeignAssetId: getForeignAssetIdFromName(name) })) as any
+
+        tokensDecimals[name] = metadata.unwrapOrDefault()?.decimals?.toNumber()
+    }
+
+    if (isStableAssetName(name) && api.query.assetRegistry && !tokensDecimals[name]) {
+        const metadata = (await api.query.assetRegistry.assetMetadatas({ StableAssetId: getStableAssetPoolIdFromName(name) })) as any
 
         tokensDecimals[name] = metadata.unwrapOrDefault()?.decimals?.toNumber()
     }
