62c05a4d18b27ac7fe8e46f6456dd98db7dbded8
qwer951123 <qiuhao951@gmail.com>
2022-04-13 11:51:48 +0800

    update

--- packages/utils/src/oracle-price.spec.ts
+++ packages/utils/src/oracle-price.spec.ts
@@ -6,7 +6,7 @@ import { queryPriceFromOracle } from './oracle-price'
 describe('all tokens helper should be ok', () => {
     // connect to acala
     const getApi = async () => {
-        return ApiPromise.create(options({ provider: new WsProvider('wss://acala-polkadot.api.onfinality.io/public-ws') }))
+        return ApiPromise.create(options({ provider: new WsProvider('wss://karura.api.onfinality.io/public-ws') }))
     }
 
     // set timeout to 60s
@@ -14,15 +14,17 @@ describe('all tokens helper should be ok', () => {
 
     test('get feed token price should be ok', async () => {
         const api = await getApi()
-        const apiAt = (await api.at('0x8175b43fd14e5a998098b330f856e895a5b247c977bd0af5d0b6a973a690a1bd')) as any
+        const apiAt = (await api.at('0x78ae7848e2ad75a328f7970acf44fb2c330a46f9d22d4c4707efd512d381b771')) as any
 
-        const block = await apiAt.rpc.chain.getBlock()
+        const block = await api.rpc.chain.getBlock()
+        
+        console.log(Object.keys(apiAt));
 
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const dotPrice = await queryPriceFromOracle(apiAt as any, block, 'DOT')
+        const lksm = await queryPriceFromOracle(apiAt as any, block, 'LKSM')
 
-        console.log(dotPrice.toString())
+        console.log(lksm.toString())
 
-        expect(dotPrice).not.toBe(FixedPointNumber.ZERO)
+        expect(lksm).not.toBe(FixedPointNumber.ZERO)
     })
 })
