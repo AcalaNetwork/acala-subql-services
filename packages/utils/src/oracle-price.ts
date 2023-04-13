579370583e87e52707b149041199394d20099ec0
nsso <nxiiun@gmail.com>
2023-04-14 11:30:36 +1200

    compatible with older versions

--- packages/utils/src/oracle-price.ts
+++ packages/utils/src/oracle-price.ts
@@ -8,9 +8,16 @@ const cache = new CacheDate<FixedPointNumber>()
 
 // query price via oracle feed
 const queryFeedPriceFromOracle = async (api: AnyApi, token: MaybeCurrency) => {
-    const result = await api.query.acalaOracle.values(forceToCurrencyId(api as any, token));
+    if(api?.query?.acalaOracle?.values) {
+        const result = await api.query.acalaOracle.values(forceToCurrencyId(api as any, token));
 
-    return FixedPointNumber.fromInner(result?.value?.value?.toString() || result?.value?.toString() || 0, 18);
+        return FixedPointNumber.fromInner(result?.value?.value?.toString() || result?.value?.toString() || 0, 18)
+    } else {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const result = (await (api.rpc as any).oracle.getValue('Aggregated', forceToCurrencyId(api, token))) as any
+    
+        return FixedPointNumber.fromInner(result?.value?.value?.toString() || result?.value?.toString() || 0, 18)
+    }
 }
 
 // query liquid token price via homa lite
